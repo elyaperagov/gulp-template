7d2a4b5723796290fcba2fb01ccac690d99b474b
Елья <piragof@gmail.com>
2019-12-30 14:47:10 +0300

    доделать localStorage

--- build/js/script.js
+++ build/js/script.js
@@ -3,18 +3,19 @@ var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 
 var modal = document.querySelector('.modal');
+var modalFrame = document.querySelector('.modal__frame')
 var modalOpen = document.querySelector('.button--contacts');
 var modalClose = modal.querySelector('.modal__close');
 var contentsInner = document.querySelector('.contents__inner');
 var contents = document.querySelector('.contents');
 var contacts = document.querySelector('.footer-contacts');
 var contactsInner = document.querySelector('.footer-contacts__inner');
-var check = modal.querySelector('input[type="checkbox"]');
 var modalName = modal.querySelector('input[type="text"]');
 var modalTel = modal.querySelector('input[type="tel"]');
 var modalQuestion = modal.querySelector('#modal-text');
 
 var saveInLocalStorage = function () {
+  if (modalName.value && modalTel.value && modalQuestion.value) {
   var data = {
     name: modalName.value,
     telephone: modalTel.value,
@@ -22,11 +23,31 @@ var saveInLocalStorage = function () {
   };
 
   localStorage.setItem('data', JSON.stringify(data));
+  // console.log(data);
+  // var info = JSON.parse(localStorage.getItem('data'));
+  };
+}
+
+var checkValidFields = function(a, b, c) {
+  a.onblur = function () {
+  if (a.value != null) {
+    saveInLocalStorage();
+    }
+  }
+  b.onblur = function () {
+  if (b.value != null) {
+    saveInLocalStorage();
+    }
+  }
+  c.onblur = function () {
+  if (c.value != null) {
+    saveInLocalStorage();
+    }
+  }
 };
 
-check.addEventListener('click', function () {
-  saveInLocalStorage();
-});
+checkValidFields(modalName, modalTel, modalQuestion);
+
 
 window.setTimeout(function () {
   localStorage.removeItem('data');
@@ -60,6 +81,8 @@ var onPopupEscPress = function (evt) {
 var openPopup = function () {
   modal.classList.remove('hidden');
   modalName.focus();
+  modalClose.addEventListener('click', closePopup);
+  modalFrame.addEventListener('click', closePopup);
   document.addEventListener('keydown', onPopupEscPress);
   document.body.style.overflow = 'hidden';
 };
@@ -67,6 +90,8 @@ var openPopup = function () {
 var closePopup = function () {
   modal.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
+  modalClose.removeEventListener('click', closePopup);
+  modalFrame.removeEventListener('click', closePopup);
   document.body.style.overflow = 'visible';
 };
 
@@ -80,7 +105,7 @@ modalOpen.addEventListener('keydown', function (evt) {
   }
 });
 
-modalClose.addEventListener('click', function () {
+modalClose.addEventListener('click', function (evt) {
   closePopup();
 });
 
