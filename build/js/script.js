69e9cf1820f479e0bbefff571035902ef5294391
Елья <piragof@gmail.com>
2020-01-22 02:21:01 +0300

    исправляет некоторые замечания

--- build/js/script.js
+++ build/js/script.js
@@ -15,15 +15,60 @@ var modalTel = modal.querySelector('input[type="tel"]');
 var modalQuestion = modal.querySelector('#modal-text');
 var info = document.querySelector('.button--info');
 var form = document.querySelector('.form');
+var scroll = document.querySelector('.info__scroll');
+var about = document.querySelector('.about');
 
 
-var handleButtonClick = function () {
+info.addEventListener('click', function () {
   form.scrollIntoView({
     block: 'center', behavior: 'smooth'}
   );
-};
+});
 
-info.addEventListener('click', handleButtonClick);
+scroll.addEventListener('click', function () {
+  about.scrollIntoView({
+    block: 'center', behavior: 'smooth'}
+  );
+});
+
+/*
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('#phone'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});*/
+
 
 var saveInLocalStorage = function () {
   if (modalName.value && modalTel.value && modalQuestion.value) {
